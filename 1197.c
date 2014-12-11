28f813a118b9ef6f9cfcdfde563d740f053d8270
Artem Vorotnikov <artem@vorotnikov.me>
2014-12-11 05:19:33 +0300

    1197 - алгоритм

--- 1197.c
+++ 1197.c
@@ -6,7 +6,7 @@ int main()
     int xpos; //вертикаль a-h => 0-7
     int ypos_human;// горизонталь 1-8
     int ypos; //горизонталь 0-7
-    int movenum;
+    int movenum=0;
 
     int N;
     for (N=0;N<100;N++)
@@ -16,12 +16,44 @@ int main()
         ypos = ypos_human-1;
         if(xpos>=0&&ypos>=0&&xpos<=7&&ypos<=7)
         {
-            printf("Location: %d : %d\n",xpos,ypos);
+            if(xpos+2<=7&&ypos+1<=7)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos+1<=7&&ypos+2<=7)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos-2>=0&&ypos+1<=7)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos+1<=7&&ypos-2<=7)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos+2<=7&&ypos-1>=0)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos-1>=0&&ypos+2<=7)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos-2>=0&&ypos-1>=0)
+            {
+                movenum=movenum+1;
+            }
+            if(xpos-1>=0&&ypos-2>=0)
+            {
+                movenum=movenum+1;
+            }
+            printf("%d\n",movenum);
         }
+        movenum=0;
         xpos_human=-1;
         ypos_human=-1;
     }
-
 
     return 0;
 }
