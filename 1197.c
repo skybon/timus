1b18ea1c57f01974ab7a7435b380b14dc6c1b2ac
Artem Vorotnikov <artem@vorotnikov.me>
2014-12-12 18:28:10 +0300

    Update 1197.c

--- 1197.c
+++ 1197.c
@@ -2,58 +2,53 @@
 
 int main()
 {
-    char xpos_human; //вертикаль a-h
-    int xpos; //вертикаль a-h => 0-7
-    int ypos_human;// горизонталь 1-8
-    int ypos; //горизонталь 0-7
-    int movenum=0;
+    int debug=0;
+    int debug_input=0;
+    int N=100;
+    if(1==debug){N=4;}
+    char xpos_human[N]; //вертикаль a-h
+    int ypos_human[N];// горизонталь 1-8
+    int pos[N][2]; //координата N:x и N:y
+    int movenum[N];
 
-    int N;
-    for (N=0;N<100;N++)
+    int i;
+    int j;
+    int k;
+
+    for (i=0;i<N;i++)
     {
-        if (0==scanf("%c%d",&xpos_human,&ypos_human)) break;
-        xpos = xpos_human-'a';
-        ypos = ypos_human-1;
-        if(xpos>=0&&ypos>=0&&xpos<=7&&ypos<=7)
-        {
-            if(xpos+2<=7&&ypos+1<=7)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos+1<=7&&ypos+2<=7)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos-2>=0&&ypos+1<=7)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos+1<=7&&ypos-2<=7)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos+2<=7&&ypos-1>=0)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos-1>=0&&ypos+2<=7)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos-2>=0&&ypos-1>=0)
-            {
-                movenum=movenum+1;
-            }
-            if(xpos-1>=0&&ypos-2>=0)
-            {
-                movenum=movenum+1;
-            }
-            printf("%d\n",movenum);
-        }
-        movenum=0;
-        xpos_human=-1;
-        ypos_human=-1;
+        pos[i][0]=0;pos[i][1]=1;ypos_human[i]=0;
+        if(EOF==scanf("\n%c%d",&xpos_human[i],&ypos_human[i])){break;}
+        if(1==debug_input){printf("i=[%d] ; xh:%c ; yh:%d\n",i,xpos_human[i],ypos_human[i]);}
+        pos[i][0] = xpos_human[i]-'a';
+        pos[i][1] = ypos_human[i]-1;
     }
+
+    if(1==debug_input)
+    {
+        for (j=0;j<i;j++)
+        {
+            printf("x[%d] : %d ; y[%d] : %d\n",j,pos[j][0],j,pos[j][1]);
+        }
+        return 0;
+    }
+
+    for (j=0;j<N;j++)
+    {
+        if(pos[j][0]>=0&&pos[j][1]>=0&&pos[j][0]<=7&&pos[j][1]<=7)
+        {
+            movenum[j]=0;
+            if(pos[j][0]+2<=7&&pos[j][1]+1<=7) {movenum[j]++;}
+            if(pos[j][0]+1<=7&&pos[j][1]+2<=7) {movenum[j]++;}
+            if(pos[j][0]-2>=0&&pos[j][1]+1<=7) {movenum[j]++;}
+            if(pos[j][0]+1<=7&&pos[j][1]-2>=0) {movenum[j]++;}
+            if(pos[j][0]+2<=7&&pos[j][1]-1>=0) {movenum[j]++;}
+            if(pos[j][0]-1>=0&&pos[j][1]+2<=7) {movenum[j]++;}
+            if(pos[j][0]-2>=0&&pos[j][1]-1>=0) {movenum[j]++;}
+            if(pos[j][0]-1>=0&&pos[j][1]-2>=0) {movenum[j]++;}
+        }
+    }
+    for(k=0;k<i;k++) {if(movenum[k]>=1&&movenum[k]<=8){printf("%d\n",movenum[k]);}}
 
     return 0;
 }
