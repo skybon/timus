c9578babc575ade9446f27c541e8d6c2b5b112d1
Artem Vorotnikov <artem@vorotnikov.me>
2015-01-08 07:19:37 +0300

    1086 - решено

--- 1086.c
+++ 1086.c
@@ -6,30 +6,40 @@ int N;
 
 int main()
 {
-	scanf("%d",&N);
-
+	scanf("%d\n",&N);
 
 	int prime_index[N];
-	int prime[N];
-
 
 	for (int i=0;i<N;i++) {scanf("%d", &prime_index[i]);}
 
-	for (int j1=0;j1<N;j1++)
+	int max_prime_index=0;
+
+	for (int i2=0;i2<N;i2++)
 	{
-		prime[j1]=2;
-		int j2=1;
-		while (j2<prime_index[j1])
+		if (prime_index[i2]>max_prime_index) {max_prime_index=prime_index[i2];}
+	}
+
+	int prime_table[max_prime_index];
+	prime_table[0]=2;
+
+	int test_var=1;
+
+	for (int j=1;j<max_prime_index;j++)
+	{
+		for (;;)																		//беск цикл чекает до прерывания
 		{
-			prime[j1]++;
+			test_var++;
+			int div=0;
 			int div_num=0;
-			for (int divisor=1;divisor<=sqrt(prime[j1]);divisor++)
+			while(div<sqrt(test_var))													//повторять до корня test_var
 			{
-				if (prime[j1]%divisor==0){div_num++;}
+				div++;
+				if (test_var%div==0) {div_num++;}
+				if (div_num>1) {break;}													//если больше чем 1 делитель - проверять не нужно - число сложное
 			}
-			if (div_num==1){j2++;}
+			if (div_num==1) {prime_table[j]=test_var;break;}
 		}
 	}
 
-	for (int k=0;k<N;k++) {printf("%d\n",prime[k]);}
+	for (int k=0;k<N;k++) {int k1=prime_index[k]-1;printf("%d\n",prime_table[k1]);}
 }
